daad1dca745a127c24aa2e8c95180dc530bc9f68
Никита <79624445+SavenkoNikita@users.noreply.github.com>
2023-11-21 07:24:13 +0300

    Добавил описание функциям

--- hh.py
+++ hh.py
@@ -10,18 +10,29 @@ from bs4 import BeautifulSoup
 # Эти параметры задаются по ссылке https://spb.hh.ru/search/vacancy?text=python&area=1&area=2
 
 def get_response():
+    """
+    Парсит страницу hh с поиском по "Python" и городами "Москва" и "Санкт-Петербург"
+    :return:массив HTML
+    """
+
     parse_link = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
     headers_gen = fake_headers.Headers(browser='firefox', os='win')
 
     html_data = requests.get(url=parse_link, headers=headers_gen.generate())
     # print(html_data.text)
-    print(html_data.status_code)
+    # print(html_data.status_code)
 
     job_search(raw_markup=html_data)
 
 
 # 2. Нужно выбрать те вакансии, у которых в описании есть ключевые слова "Django" и "Flask".
 def job_search(raw_markup):
+    """
+    Из HTML-массива извлекается каждая ссылка на вакансию и передаётся в parse_link_vacancy()
+    :param raw_markup: HTML-массив
+    :return: Список со списками, содержащие данные по вакансиям, удовлетворяющие ключевым словам
+    """
+
     list_search = ['Django', 'Flask']
 
     soup = BeautifulSoup(raw_markup.text, features='html.parser')
@@ -32,7 +43,7 @@ def job_search(raw_markup):
     # name_tag = posts.find_all(class_='serp-item__title')
     name_tag = posts.find_all(name='a', class_='serp-item__title')
 
-    suitable_data = []
+    list_of_suitable_vacancies = []
 
     for vacancy in name_tag:
         # name_vacancy = vacancy.text
@@ -42,15 +53,20 @@ def job_search(raw_markup):
         vacancy_data = parse_link_vacancy(link_vacancy)
 
         if vacancy_data is not None:
-            suitable_data.append(vacancy_data)
+            list_of_suitable_vacancies.append(vacancy_data)
         else:
             continue
+
+    return list_of_suitable_vacancies
 
 
 def parse_link_vacancy(link):
     """
     Переходит по ссылке и ищет те вакансии, у которых в описании есть ключевые слова 'Django' и 'Flask'
+    :param link: ссылка на вакансию
+    :return: если условие соблюдается - список с данными о вакансии, иначе None
     """
+
     headers_gen = fake_headers.Headers(browser='firefox', os='win')
 
     html_data = requests.get(url=link, headers=headers_gen.generate())
@@ -60,6 +76,10 @@ def parse_link_vacancy(link):
 
     # извлекаем посты
     posts = soup.find(name='main', class_='vacancy-serp-content')
+
+    answer = []
+
+    return answer
 
 
 
